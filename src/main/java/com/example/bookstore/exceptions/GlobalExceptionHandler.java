d41434b89bf04dd997622edf9759b15904809d88
yuyaapril <154661752+yuyaapril@users.noreply.github.com>
2025-11-09 23:33:08 +0630

    runtime exception

--- src/main/java/com/example/bookstore/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/bookstore/exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,17 @@
 package com.example.bookstore.exceptions;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+  @ExceptionHandler(Exception.class)
+  public String handleException(Exception e, Model model) {
+    model.addAttribute("errorMessage", e.getMessage());
+    return "error";
+  }
   
 }
