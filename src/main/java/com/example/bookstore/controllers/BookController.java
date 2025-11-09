e5f4ff29816a89e4bc9a57beb08d0319c70ade0a
yuyaapril <154661752+yuyaapril@users.noreply.github.com>
2025-11-09 13:32:25 +0630

    book-edit-filter

--- src/main/java/com/example/bookstore/controllers/BookController.java
+++ src/main/java/com/example/bookstore/controllers/BookController.java
@@ -5,7 +5,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
-
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -20,9 +20,14 @@ import com.example.bookstore.services.CatService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 
 
 
@@ -43,6 +48,8 @@ public class BookController {
   public String allBooks(Model model) {
     List<Book> books = bookService.all();
     model.addAttribute("books", books);
+     List<Category> cats = catService.getAll();
+    model.addAttribute("cats", cats);
     return "book/all";
   }
   @GetMapping("add")
@@ -82,12 +89,53 @@ public class BookController {
     book.setDescription(bookDto.getDescription());
     book.setPrice(bookDto.getPrice());
     book.setStock(bookDto.getStock());
-    
+
     Category cat = catService.getById(bookDto.getCategoryId());
     book.setCategory(cat);
     bookService.addBook(book);
-      return "redirect:/books";
+    return "redirect:/books";
   }
+  
+  @GetMapping("edit/{id}")
+  public String bookEdit(@PathVariable Long id, Model model) {
+    Book book = bookService.getById(id);
+
+    List<Category> cats = catService.getAll();
+    model.addAttribute("cats", cats);
+    model.addAttribute("book", book);
+    return "book/edit";
+  }
+  @PostMapping("edit/{id}")
+  public String editBook(@PathVariable Long id, @ModelAttribute BookDto bookDto) {
+
+    Book book = bookService.getById(id);
+
+    book.setTitle(bookDto.getTitle());
+    book.setAuthor(bookDto.getAuthor());
+    book.setPublicationDate(bookDto.getPublicationDate());
+    book.setDescription(bookDto.getDescription());
+    book.setPrice(bookDto.getPrice());
+    book.setStock(bookDto.getStock());
+    book.setCategory(catService.getById(bookDto.getCategoryId()));
+
+    if (!bookDto.getCoverImage().isEmpty()) {
+      String fileName = saveFile(bookDto.getCoverImage());
+      book.setCoverImageUrl(fileName);
+    }
+    bookService.updateBook(book);
+    return "redirect:/books";
+  }
+  
+  @GetMapping("bycat/{id}")
+  public String bookByCat(@PathVariable Integer id, Model model) {
+    Category category = catService.getById(id);
+    Set<Book> books = category.getBooks();
+    List<Category> cats = catService.getAll();
+    model.addAttribute("cats", cats);
+    model.addAttribute("books", books);
+      return "book/all";
+  }
+  
   
   
 }
